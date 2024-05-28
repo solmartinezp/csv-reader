2817b4be9b39b153e2f79f4a4d1de6ef8eb0fc79
Sol <solmartinezpisani@gmail.com>
2024-05-28 14:06:46 -0300

    Stop tracking all .js files

--- backend/middlewares/corsMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const corsMiddleware = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-};
-exports.default = corsMiddleware;
--- backend/middlewares/errorHandling.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Middleware to handle errors
-function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Internal server error' });
-}
-exports.default = errorHandler;
